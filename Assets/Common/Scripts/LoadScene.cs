0f2df8be64040cfcdf12c5df54880819cd9d4ac4
e-strauch <31522135+e-strauch@users.noreply.github.com>
2019-11-22 03:52:40 -0500

    Functional Scan and Map buttons
    Buttons take user to Scan and Map scenes, no current back button for either

---
+++ Assets/Common/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LoadScene : MonoBehaviour {
+    //is called before the first frame update
+    
+    public void changeMenuScene(string sceneName){
+    	 SceneManager.LoadScene(sceneName);
+    	 Console.WriteLine("Hello World!");
+    }
+}
